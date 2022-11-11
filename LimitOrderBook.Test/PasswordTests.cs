7492207da96e60410304318e479e3a06fc7708e7
Leon Becker <Leon.Becker@itechx.de>
2022-11-11 20:45:22 +0100

    Added unit tests for password validation

---
+++ LimitOrderBook.Test/PasswordTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitOrderBook.Test;
+
+public class PasswordTests
+{
+    [Fact]
+    public void PasswordMustContainUpperCharacter()
+    {
+        var passwordVerifyer = UtilityClass.GetPasswordVerifyer();
+        string password = "helloworld1";
+
+        Assert.False(passwordVerifyer.PasswordIsValid(password));
+    }
+
+    [Fact]
+    public void PasswordMustContainLowerCharacter()
+    {
+        var passwordVerifyer = UtilityClass.GetPasswordVerifyer();
+        string password = "HELLOWORLD1";
+
+        Assert.False(passwordVerifyer.PasswordIsValid(password));
+    }
+
+    [Fact]
+    public void PasswordMustContainNumber()
+    {
+        var passwordVerifyer = UtilityClass.GetPasswordVerifyer();
+        string password = "HelloWorld";
+
+        Assert.False(passwordVerifyer.PasswordIsValid(password));
+    }
+
+    [Fact]
+    public void PasswordMustHave8CharactersOrMore()
+    {
+        var passwordVerifyer = UtilityClass.GetPasswordVerifyer();
+        string password = "Hw1";
+
+        Assert.False(passwordVerifyer.PasswordIsValid(password));
+    }
+
+    [Fact]
+    public void ValidPasswordGetsAccepted()
+    {
+        var passwordVerifyer = UtilityClass.GetPasswordVerifyer();
+        string password = "HelloWorld1";
+
+        Assert.False(passwordVerifyer.PasswordIsValid(password));
+    }
+}
